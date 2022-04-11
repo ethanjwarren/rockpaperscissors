0ca1ef1760b6fdcae839f45a08b6843e40d30e06
Ethan Warren <ethan.warren10@gmail.com>
2022-04-11 20:03:04 +0100

    Random return of 'Rock', 'Paper', 'Scissors'

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-console.log("hello world");
+const array = ['Rock', 'Paper', 'Scissors'];
 
+function computerPlay(arr) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+
+    const item = arr[randomIndex];
+
+    return item;
+}
+
+const result = computerPlay(array);
+console.log(result);
